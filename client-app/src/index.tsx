37676e5e8d6f3acf0c47acb892ada52b310ef7c1
Anjaney <anjaneya.singh94@gmail.com>
2021-06-09 10:05:42 +0530

    Fix - calendar alignment.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "react-calendar/dist/Calendar.css";
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 import { store, storeContext } from './app/stores/store';
 import { BrowserRouter as Router} from "react-router-dom";
-import "react-calendar/dist/Calendar.css";
 
 ReactDOM.render(
   <storeContext.Provider value={store}>

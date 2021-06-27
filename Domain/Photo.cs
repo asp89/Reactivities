ed8015ef28a8e1cf3bede8d777eac717c6e4196d
Anjaney <anjaneya.singh94@gmail.com>
2021-06-27 14:12:31 +0530

    Implemented Image Upload APIs.

---
+++ Domain/Photo.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class Photo
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
